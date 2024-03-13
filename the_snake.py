e46c90e8d00546044e096f303074f2104493f7fc
Ivan Kazakov <159909538+IvanKazakovI@users.noreply.github.com>
2024-03-13 19:21:25 +0300

    changed SPEED

--- the_snake.py
+++ the_snake.py
@@ -29,7 +29,7 @@ BOARD_BACKGROUND_COLOR = (0, 0, 0)
 BORDER_COLOR = (93, 216, 228)
 APPLE_COLOR = (255, 0, 0)
 SNAKE_COLOR = (0, 255, 0)
-SPEED = int(input('Выберите скорость с которой хотите играть: '))
+SPEED = 20
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 pygame.display.set_caption(f'Змейка. Клавиши управления:'
